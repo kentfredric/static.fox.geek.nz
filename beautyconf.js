4f794b04fff61a7a89710782fb2e1f5381632046
Kent Fredric <kentfredric@gmail.com>
2010-06-28 16:42:08 +1200

    Refactored to be <pre> oriented instead of <code> oriented

--- beautyconf.js
+++ beautyconf.js
@@ -14,16 +14,43 @@ jQuery(function($){
             }
         });
     };
+    var prenodeswitch = function( match , cast ) {
+        $('pre.' + match).each(function(){
+            var node = $(this).clone();
+            node.removeClass(match);
+            var wrap = document.createElement('pre');
+            var code = document.createElement('code');
+            $(wrap).append(code);
+            $(code).append(node.contents());
+            $(this).replaceWith(wrap);
+            if( jQuery.isFunction(cast) ){
+                cast(wrap);
+            } else {
+                $(wrap).beautifyCode(cast);
+            }
+        });
+    };
 
     var format = function(){
+        prenodeswitch('perl', 'Perl');
+        prenodeswitch('bash', 'bash');
+        prenodeswitch('plaintext','plain');
+        prenodeswitch('ini','INI');
+        prenodeswitch('darkperl', function(wrap){ 
+            $(wrap).beautifyCode('Perl',{ gutter: false, "class-name": "darkperlmod" });
+        });
+    /*  
+        deprecated: <pre> works better than <code>   
         nodeswitch('code.perl','perl','Perl');
         nodeswitch('code.bash','bash','bash');
         nodeswitch('code.plaintext','plaintext','plain');
         nodeswitch('code.darkperl','darkperl',function(wrap){ 
             $(wrap).beautifyCode('Perl',{ gutter: false, "class-name": "darkperlmod" });
         });
-        nodeswitch('code.ini', 'ini', 'INI' );
-        $("div.darkperlmod div.lines").attr('style','background-color: rgba(200,255,200.0.5) !important');
+        nodeswitch('code.ini', 'ini', 'INI' ); */
+        $("div.darkperlmod div.lines").attr('style','background-color: rgba(200,255,200,0.5) !important');
+        $("div.darkperlmod .alt1 .content").attr('style','background-color: #DDDDDD !important');
+        $("div.darkperlmod .alt2 .content").attr('style','background-color: #D8D8D8 !important');
     };
 
     var config = {
